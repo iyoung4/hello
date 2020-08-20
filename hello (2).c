fe6cd507d452c73eaf1f0d6472e4c9b161071597
iyoung4 <69777317+iyoung4@users.noreply.github.com>
2020-08-20 12:26:53 -0500

    I added my major to the output.

--- hello (2).c
+++ hello (2).c
@@ -1,16 +1,17 @@
 /**
  * Author: Isaac Young
  * Date:   2020
- * 
+ *
  * A simple hello world program
  */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    
+
     printf("Isaac Young\n");
-    
+    printf("Actuarial Science\n");
+
     return 0;
-    
+
 }
