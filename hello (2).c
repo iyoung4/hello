671fd04ca7263114c0760049c4b3f77a4a9ee4b9
iyoung4 <69777317+iyoung4@users.noreply.github.com>
2020-08-20 12:15:10 -0500

    Initial commit

---
+++ hello (2).c
@@ -0,0 +1,16 @@
+/**
+ * Author: Isaac Young
+ * Date:   2020
+ * 
+ * A simple hello world program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("Isaac Young\n");
+    
+    return 0;
+    
+}
